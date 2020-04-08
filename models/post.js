cafaf7a9c1de0511e0ff1541fa8326f6e5557684
Brian Wilson <b@Brians-MacBook-Pro.local>
2020-04-08 14:23:45 -0500

    added post Schema

---
+++ models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+
+const postSchema = new mongoose.Schema({
+  city: String,
+  country: String,
+  date: Date,
+  postDescription: String,
+  postPicture: String,
+  traveler: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true 
+  },
+  comments: [Comment.schema]
+})
+
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = Post
