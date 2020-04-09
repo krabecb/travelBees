4fce20b1862b1304e620abeb98ba435ff4c247bd
Brian Wilson <b@Brians-MacBook-Pro.local>
2020-04-09 11:22:25 -0500

    comment model setup

---
+++ models/comment.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+	text: String,
+	date: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	}
+})
+
+const Comment = mongoose.model('Comment', commentSchema)
+
+module.exports = Comment
